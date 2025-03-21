00ca1711757c6fc26d3a049777c82168639d1552
watorutart <watoru374215@gmail.com>
2025-03-21 22:33:38 +0900

    feat: :sparkles: time..Nowをお試し

---
+++ 01_using_the_tour/sandbox.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main () {
+	// Print "Hello, World!" to the console
+	fmt.Println("Hello, World!")
+
+	// Print the current time
+	fmt.Println("Current time:", time.Now())
+}
