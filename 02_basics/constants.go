23512179125a318305f900834b89b6edb092c70e
watorutart <watoru374215@gmail.com>
2025-03-22 12:47:16 +0900

    feat: :sparkles: Constantsを写経

---
+++ 02_basics/constants.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+// 定数定義。定数定義は:=を使えない
+const Pi = 3.14
+
+func main() {
+	const World = "世界"
+	fmt.Println("Hello", World)
+	fmt.Println("Happy", Pi, "Day")
+
+	const Truth = true
+	fmt.Println("Go rules?", Truth)
+}
