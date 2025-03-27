200b75f5c5e7007a3353fde9be084abd1e6ab73a
watorutart <watoru374215@gmail.com>
2025-03-27 21:29:56 +0900

    feat: :sparkles: Rangeを写経

---
+++ 04_more_types/range.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// 括弧でなく波括弧なのが注意点
+var pow = []int{1, 2, 4, 8, 16, 32, 64, 128}
+
+func main() {
+	for i, v := range pow {
+		fmt.Printf("2**%d = %d\n", i, v)
+	}
+}
