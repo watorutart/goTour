4f4702ec3d5ae587d51d68f69870a7ca774b96b3
watorutart <watoru374215@gmail.com>
2025-03-29 09:47:21 +0900

    feat: :sparkles: Exercise: slicesをお試し

---
+++ 04_more_types/exercise-slices.go
@@ -0,0 +1,19 @@
+package main
+
+import "golang.org/x/tour/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+	pic := make([][]uint8, dy)
+	for i := range pic {
+		pic[i] = make([]uint8, dx)
+		for j := range pic[i] {
+			// 画像生成のアルゴリズム設定
+			pic[i][j] = uint8(i ^ j)
+		}
+	}
+	return pic
+}
+
+func main() {
+	pic.Show(Pic)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go-qr-app
 
 go 1.24.1
+
+require golang.org/x/tour v0.1.0 // indirect
