7041926876bf4dc832f6292089add9b23c983f8e
watorutart <watoru374215@gmail.com>
2025-04-01 21:26:39 +0900

    feat: :sparkles: map-literals-continuedを写経

---
+++ 04_more_types/map-literals-continued.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+type Vertex struct {
+	Lat, Long float64
+}
+
+// mapに渡すトップレベルの型が単純な型名である場合はリテラルの要素から推定可能
+var m = map[string]Vertex{
+	"Bell Labs": Vertex{40.68433, -74.39967},
+	"Google": Vertex{37.42202, -122.08408},
+}
+
+func main() {
+	fmt.Println(m)
+}
