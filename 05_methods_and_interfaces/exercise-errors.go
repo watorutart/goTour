8222db58ac9abdd6e2109240ed351713a6858c45
watorutart <watoru374215@gmail.com>
2025-04-18 21:52:13 +0900

    feat: :sparkles: exercise-errorsを一旦コピー

---
+++ 05_methods_and_interfaces/exercise-errors.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+// TODO: exercise実施前
+func Sqrt(x float64) (float64, error) {
+	return 0, nil
+}
+
+func main() {
+	fmt.Println(Sqrt(2))
+	fmt.Println(Sqrt(-2))
+}
