0434e3b2e83cef15e88d4f54492af059ea8906d3
watorutart <watoru374215@gmail.com>
2025-04-19 14:27:29 +0900

    feat: :sparkles: Exercise: Errorsを実装

--- 05_methods_and_interfaces/exercise-errors.go
+++ 05_methods_and_interfaces/exercise-errors.go
@@ -4,8 +4,17 @@ import (
 	"fmt"
 )
 
+type ErrNegativeSqrt float64
+
+func (e ErrNegativeSqrt) Error() string {
+	return fmt.Sprintf("cannot Sqrt negative number: %v", float64(e))
+}
+
 // TODO: exercise実施前
 func Sqrt(x float64) (float64, error) {
+	if (x < 0) {
+		return 0, ErrNegativeSqrt(x)
+	}
 	return 0, nil
 }
 
