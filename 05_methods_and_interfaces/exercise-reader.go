9a460b894417daf7e9fc39fb18c1af28cab86a42
watorutart <watoru374215@gmail.com>
2025-04-22 22:40:01 +0900

    feat: :sparkles: Exercise Readerを写経

--- 05_methods_and_interfaces/exercise-reader.go
+++ 05_methods_and_interfaces/exercise-reader.go
@@ -5,6 +5,9 @@ import "golang.org/x/tour/reader"
 type MyReader struct{}
 
 // TODO: Add a Read([]byte) (int, error) method to MyReader.
+func (MyReader) Read([]byte) (int, error) {
+	return 0, nil
+}
 
 func main () {
 	reader.Validate(MyReader{})
