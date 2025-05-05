107661cef55370ecf71b0d144813371da5557644
watorutart <watoru374215@gmail.com>
2025-05-05 21:42:56 +0900

    feat: :sparkles: Walk関数を実装

--- 06_concurrency/exercise-equivalent-binary-trees.go
+++ 06_concurrency/exercise-equivalent-binary-trees.go
@@ -4,12 +4,23 @@ import "golang.org/x/tour/tree"
 
 // Walk walks the tree t sending all values
 // from the tree to the channel ch.
-func Walk(t *tree.Tree, ch chan int)
+func Walk(t *tree.Tree, ch chan int) {
+	// Walk the tree in-order and send values to ch
+	if t != nil {
+		Walk(t.Left, ch)
+		ch <- t.Value
+		Walk(t.Right, ch)
+	}
+}
 
 // Same determines whether the trees
 // t1 and t2 contain the same values.
-func Same(t1, t2 *tree.Tree) bool
+func Same(t1, t2 *tree.Tree) bool {
+	// TODO: Same関数を実装
+	return false
+}
 
 func main() {
-	// TODO: Implement Walk and Same
+	ch := make(chan int)
+	go Walk(tree.New(1), ch)
 }
