04e17d67d2cac1777ab37e7a99a67083dff744ce
watorutart <watoru374215@gmail.com>
2025-05-06 08:32:46 +0900

    feat: :sparkles: Same関数を検討中

--- 06_concurrency/exercise-equivalent-binary-trees.go
+++ 06_concurrency/exercise-equivalent-binary-trees.go
@@ -1,12 +1,16 @@
 package main
 
-import "golang.org/x/tour/tree"
+import (
+	"golang.org/x/tour/tree"
+	"fmt"
+)
 
 // Walk walks the tree t sending all values
 // from the tree to the channel ch.
 func Walk(t *tree.Tree, ch chan int) {
 	// Walk the tree in-order and send values to ch
 	if t != nil {
+		fmt.Println("Walked tree:", t.Value)
 		Walk(t.Left, ch)
 		ch <- t.Value
 		Walk(t.Right, ch)
@@ -23,4 +27,6 @@ func Same(t1, t2 *tree.Tree) bool {
 func main() {
 	ch := make(chan int)
 	go Walk(tree.New(1), ch)
+	fmt.Println("Same:", Same(tree.New(1), tree.New(1)))
+	fmt.Println("Same:", Same(tree.New(1), tree.New(2)))
 }
